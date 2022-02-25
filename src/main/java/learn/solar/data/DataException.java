1dadb4cc230ee832a0500829a167d73ec43872a7
njsutliff <njsutliff@gmail.com>
2022-02-25 16:15:43 -0500

    Panel, Material, DataException implemented

--- src/main/java/learn/solar/data/DataException.java
+++ src/main/java/learn/solar/data/DataException.java
@@ -1,4 +1,16 @@
 package learn.solar.data;
+    public class DataException extends Exception {
 
-public class DataException {
-}
+        public DataException(String message) {
+            super(message);
+        }
+
+        public DataException(Throwable cause) {
+            super(cause);
+        }
+
+        public DataException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+
--- src/main/java/learn/solar/models/Panel.java
+++ src/main/java/learn/solar/models/Panel.java
@@ -1,4 +1,84 @@
 package learn.solar.models;
 
+import java.util.Objects;
+
 public class Panel {
+    private int id;
+    private String section;
+    private int row;
+    private int column;
+    private int installationYear;
+    private Material material;
+    private boolean tracking;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getInstallationYear() {
+        return installationYear;
+    }
+
+    public void setInstallationYear(int installationYear) {
+        this.installationYear = installationYear;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public boolean isTracking() {
+        return tracking;
+    }
+
+    public void setTracking(boolean tracking) {
+        this.tracking = tracking;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Panel panel = (Panel) o;
+        return id == panel.id && row == panel.row && column == panel.column
+                && installationYear == panel.installationYear && tracking == panel.tracking
+                && section.equals(panel.section) && material == panel.material;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, section, row, column, installationYear, material, tracking);
+    }
 }
