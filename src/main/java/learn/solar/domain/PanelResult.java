09c95439796b665e42cbf7f7376e8740db641de2
njsutliff <njsutliff@gmail.com>
2022-02-27 13:04:32 -0500

    panelresult, panelservice done except validate

--- src/main/java/learn/solar/domain/PanelResult.java
+++ src/main/java/learn/solar/domain/PanelResult.java
@@ -1,4 +1,41 @@
 package learn.solar.domain;
 
+import learn.solar.models.Panel;
+
+import java.util.ArrayList;
+
 public class PanelResult {
+    private ArrayList<String> messages;
+    private Panel panel;
+    public ArrayList<String> getMessages() {
+         return new ArrayList<>(messages);
+    }
+
+    public Panel getPanel() {
+        return panel;
+    }
+
+    public void setPanel(Panel panel) {
+        this.panel = panel;
+    }
+
+    public boolean isSuccess() {
+        return messages.size() == 0;
+    }
+
+    public void addMessage(String message) {
+        messages.add(message);
+    }
+/* //TODO may need hashCode and equals for PanelResult
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+    */
+
 }
--- src/main/java/learn/solar/domain/PanelService.java
+++ src/main/java/learn/solar/domain/PanelService.java
@@ -1,4 +1,57 @@
 package learn.solar.domain;
 
+import learn.solar.data.DataException;
+import learn.solar.data.PanelFileRepository;
+import learn.solar.data.PanelRepository;
+import learn.solar.models.Panel;
+
+import java.util.List;
+
 public class PanelService {
+
+    private PanelRepository repository;
+
+    public PanelService(PanelRepository repository){
+        this.repository = repository;
+    }
+    public List<Panel> findBySection(String section) throws DataException {
+        return repository.findBySection(section);
+    }
+
+    public  PanelResult add(Panel p) throws DataException{
+        PanelResult result = validate(p);
+        if(!result.isSuccess()){
+            return result;
+        }
+        result.setPanel(p);
+        repository.add(p);
+        return  result;
+    }
+    public PanelResult update(Panel p) throws  DataException {
+        PanelResult result = validate(p);
+        if(p.getId()<=0 ||p.getId() >=250){
+            result.addMessage("ID invalid to place in solar panel array. ");
+            return result;
+        }
+        if(!repository.update(p)){
+            result.addMessage(String.format("Failed to update Panel id row col: ", p.getId(), p.getRow(),p.getColumn()));
+        }
+
+        return  result;
+    }
+    public  PanelResult deleteById(int Id) throws  DataException {
+        PanelResult result = new PanelResult();
+        if(!repository.deleteById(Id)){
+            result.addMessage(String.format("Failed to delete panel with id", Id));
+            return result;
+        }
+        return  result;
+    }
+    private  PanelResult validate(Panel p) {
+        PanelResult result = new PanelResult();
+        return  result;
+    }
+
+
+
 }
