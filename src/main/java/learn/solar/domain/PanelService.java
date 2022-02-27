6c9bbbbdac958a50a0706f8a2348ef7636cd40ee
njsutliff <njsutliff@gmail.com>
2022-02-27 15:56:50 -0500

    PanelService done, now time for testing

--- src/main/java/learn/solar/domain/PanelService.java
+++ src/main/java/learn/solar/domain/PanelService.java
@@ -3,6 +3,7 @@ package learn.solar.domain;
 import learn.solar.data.DataException;
 import learn.solar.data.PanelFileRepository;
 import learn.solar.data.PanelRepository;
+import learn.solar.models.Material;
 import learn.solar.models.Panel;
 
 import java.util.List;
@@ -11,47 +12,76 @@ public class PanelService {
 
     private PanelRepository repository;
 
-    public PanelService(PanelRepository repository){
+    public PanelService(PanelRepository repository) {
         this.repository = repository;
     }
+
     public List<Panel> findBySection(String section) throws DataException {
         return repository.findBySection(section);
     }
 
-    public  PanelResult add(Panel p) throws DataException{
+    public PanelResult add(Panel p) throws DataException {
         PanelResult result = validate(p);
-        if(!result.isSuccess()){
+        if (!result.isSuccess()) {
             return result;
         }
         result.setPanel(p);
         repository.add(p);
-        return  result;
+        return result;
     }
-    public PanelResult update(Panel p) throws  DataException {
-        PanelResult result = validate(p);
-        if(p.getId()<=0 ||p.getId() >=250){
-            result.addMessage("ID invalid to place in solar panel array. ");
-            return result;
-        }
-        if(!repository.update(p)){
-            result.addMessage(String.format("Failed to update Panel id row col: ", p.getId(), p.getRow(),p.getColumn()));
-        }
 
-        return  result;
+    public PanelResult update(Panel p) throws DataException {
+        PanelResult result = validate(p);
+        if (!repository.update(p)) {
+            result.addMessage(String.format("Failed to update Panel id row col: ", p.getId(), p.getRow(), p.getColumn()));
+        }
+        return result;
     }
-    public  PanelResult deleteById(int Id) throws  DataException {
+
+    public PanelResult deleteById(int Id) throws DataException {
         PanelResult result = new PanelResult();
-        if(!repository.deleteById(Id)){
+        if (!repository.deleteById(Id)) {
             result.addMessage(String.format("Failed to delete panel with id", Id));
             return result;
         }
-        return  result;
-    }
-    private  PanelResult validate(Panel p) {
+        return result;
+    }// general purpose validation
+
+    private PanelResult validate(Panel p) {
         PanelResult result = new PanelResult();
-        return  result;
+
+        if (p.getSection().isBlank()) {
+            result.addMessage("Section name cannot be blank");
+            return result;
+        }
+        if (p.getId() <= 0 || p.getId() >= 250) {
+            result.addMessage("ID invalid to place in solar panel array. ");
+            return result;
+        }
+        if (p.getSection().isBlank()) {
+            result.addMessage("Section cannot be blank");
+            return result;
+        }
+        if (p.getRow() <= 0 || p.getRow() >= 250) {
+            result.addMessage(String.format("Row ", p.getRow(), " out of range"));
+            return result;
+        }
+        if (p.getColumn() <= 0 || p.getColumn() >= 250) {
+            result.addMessage(String.format("Column ", p.getColumn(), " out of range"));
+            return result;
+        }
+        if (p.getInstallationYear() > 2022) {
+            result.addMessage("Year cannot be in the future. ");
+        }
+        if (p.getMaterial() != Material.AMORPHOUS_SILICON
+                || p.getMaterial() != Material.CADMIUM_TELLURIDE
+                || p.getMaterial() != Material.COPPER_IRIDIUM_GALLIUM_SELENIDE
+                || p.getMaterial() != Material.MONOCRYSTALINE_SILICON
+                || p.getMaterial() != Material.MULTICRYSTALLINE_SILICON
+                || p.getMaterial() == null) {
+            result.addMessage(String.format("Material must be one of the required materials, not ", p.getMaterial()));
+            return result;
+        }
+        return result;
     }
-
-
-
 }
